7b1c31829ba321b6c8ca3f94a26f896ea4b0d110
Nendra12 <ardhidanendra123@gmail.com>
2026-03-04 11:03:20 +0700

    Menambah api statistik jawaban kuesioner

--- database/migrations/2026_02_20_000021_create_pertanyaan_kuesioner_table.php
+++ database/migrations/2026_02_20_000021_create_pertanyaan_kuesioner_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('pertanyaan_kuesioner', function (Blueprint $table) {
             $table->id('id_pertanyaanKuis');
             $table->unsignedBigInteger('id_kuesioner');
-            $table->text('pertanyaan');
+            $table->longText('pertanyaan');
             $table->foreign('id_kuesioner')->references('id_kuesioner')->on('kuesioner')->onDelete('cascade');
             $table->timestamps();
         });
