efb28372f472abb6cf12417efda6ec350ecd8373
Nendra12 <ardhidanendra123@gmail.com>
2026-06-05 15:53:29 +0700

    fix error cors

--- config/cors.php
+++ config/cors.php
@@ -8,6 +8,7 @@ return [
         'http://localhost:5174',          // Alternative local port
         'https://api-tracer.primaesemkrada.com', // Production API
         'https://tracer.primaesemkrada.com', // Production frontend
+        'https://tracer.hummatech.com',
         'https://c4c3-139-228-40-5.ngrok-free.app' // ngrok
     ],
     'allowed_origins_patterns' => [],
